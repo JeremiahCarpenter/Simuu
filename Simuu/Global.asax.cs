5b99a404151e35e0f5b42e3c86620db1702660ef
SeanLeeDev <33349300+SeanLeeDev@users.noreply.github.com>
2019-11-06 15:41:45 -0600

    Add files via upload

---
+++ Simuu/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Security.Principal;
+
+namespace Simuu
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+
+        protected void Application_Start()
+        {
+            ApplicationConfig.RegisterAplicationVariables();
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var ex = Server.GetLastError();
+            if (ex is System.Threading.ThreadAbortException)
+                return; // Redirects may cause this exception..
+            LoggingLayer.Logger.Log(ex);
+        }
+
+
+        protected void Application_AcquireRequestState()
+        {
+            string sessionUser = Session["AUTHUser"] as string;
+            string sessionRole = Session["AUTHRole"] as string;
+            string validationType = Session["AUTHTYPE"] as string;
+            if (string.IsNullOrEmpty(sessionUser))
+            {
+                return;
+            }
+            GenericIdentity identity = new GenericIdentity(sessionUser, validationType);
+            if (sessionRole == null) { sessionRole = ""; }
+            string[] roles = sessionRole.Split(' ');
+            GenericPrincipal p = new GenericPrincipal(identity, roles);
+            HttpContext.Current.User = p;
+        }
+
+    }
+}
---
+++ TestingLayer/SimulationLogicTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessLogicLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestingLayer
+{
+    [TestClass]
+    public class SimulationLogicTests
+    {
+        List<SimuuBLL> MakeSampleSimuus(int count)
+        {
+            List<SimuuBLL> proposedReturnValue = new List<SimuuBLL>();
+            for (int i = 0; i < count; i++)
+            {
+                SimuuBLL simuu = new SimuuBLL();
+                simuu.SimuuID = i;
+                simuu.SimuuName = $"Simuu{i}";
+                simuu.SimuuAge = 5 + i;
+                simuu.SimuuBirth = new DateTime(1900 + i * 5, 1, 1);
+                simuu.SimuuDeath = new DateTime(1900 + i * 5, 1, 1);
+                simuu.SimuuXCoordinate = 55;
+                simuu.SimuuYCoordinate = 55;
+                simuu.ImpulseToRest = 25;
+                simuu.ImpulseToDrink = 25;
+                simuu.ImpulseToEat = 25;
+                simuu.StatEnergy = 100;
+                simuu.StatThirst = 100;
+                simuu.StatHunger = 100;
+                simuu.SimuuMovementSpeed = 5;
+                simuu.SimuuSenseRadius = 15;
+                simuu.UserID = i % 3;
+                proposedReturnValue.Add(simuu);
+            }
+            return proposedReturnValue;
+        }
+
+        SimuuBLL MakeSampleSimuu(int i)
+        {
+            SimuuBLL simuu = new SimuuBLL();
+            simuu.SimuuID = i;
+            simuu.SimuuName = $"Simuu{i}";
+            simuu.SimuuAge = 5 + i;
+            simuu.SimuuBirth = new DateTime(1900 + i * 5, 1, 1);
+            simuu.SimuuDeath = new DateTime(1900 + i * 5, 1, 1);
+            simuu.SimuuXCoordinate = 55;
+            simuu.SimuuYCoordinate = 55;
+            simuu.ImpulseToRest = 25;
+            simuu.ImpulseToDrink = 25;
+            simuu.ImpulseToEat = 25;
+            simuu.StatEnergy = 100;
+            simuu.StatThirst = 100;
+            simuu.StatHunger = 100;
+            simuu.SimuuMovementSpeed = 5;
+            simuu.SimuuSenseRadius = 15;
+            simuu.UserID = i % 3;
+            return simuu;
+        }
+
+        [TestMethod]
+        public void When_SimuuSensesSimuu_Expect_True()
+        {
+            // arrange
+            SimulationLogic logic = new SimulationLogic();
+            SimuuBLL simuu1 = MakeSampleSimuu(1);
+            SimuuBLL simuu2 = MakeSampleSimuu(2);
+
+            bool expected = true;
+            // act
+            bool actual = logic.TestSenseCollision(simuu1, simuu2);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void When_SimuuDoesntSenseSimuu_Expect_False()
+        {
+            // arrange
+            SimulationLogic logic = new SimulationLogic();
+            SimuuBLL simuu1 = MakeSampleSimuu(1);
+            SimuuBLL simuu2 = MakeSampleSimuu(2);
+            simuu2.SimuuXCoordinate = 100;
+            simuu2.SimuuYCoordinate = 100;
+
+            bool expected = false;
+            // act
+            bool actual = logic.TestSenseCollision(simuu1, simuu2);
+
+            //assert
+            Assert.AreNotEqual(expected, actual);
+        }
+
+    }
+}
